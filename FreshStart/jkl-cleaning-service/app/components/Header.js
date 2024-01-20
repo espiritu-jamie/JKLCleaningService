e9c3e388d7b96fec540d1ebfab86a9fc71dd2c46
navrajsingh4 <119980499+navrajsingh4@users.noreply.github.com>
2024-01-19 19:00:55 -0700

    edited

--- FreshStart/jkl-cleaning-service/app/components/Header.js
+++ FreshStart/jkl-cleaning-service/app/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../css/header.css'
+import logoImage from '/app/assets/logo.png';
+
 const Header = () => {
   return (
     <div>
       <header>
         <nav className="box-container">
-          <div className="logodiv"><img src="app\assets\logo.png" /></div>
+          <div className="logodiv"><img src={logoImage} /></div>
           <div className="namediv">
             <h1> JKL Cleaning Service</h1>
             <h5> We Treat you house like ours</h5>
